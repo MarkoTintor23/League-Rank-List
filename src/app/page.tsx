268095e76f2df1288b8b4f3ba214f2333c77eb57
Marko Tintor <marko.tintor.18@singimail.rs>
2025-10-17 22:49:44 +0200

    added inter and tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
       <p>ovo je pocetak</p>
+      <h1 className="text-yellow-400 text-3xl font-bold">League Rank List</h1>
     </>
   );
 }
