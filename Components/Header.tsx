68cb8811ae7dbdc4a9a5ff17a5d02e01bb1ea2fe
Marko Tintor <marko.tintor.18@singimail.rs>
2025-10-19 12:24:06 +0200

    changed react with nextjs

---
+++ Components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <>
+      <header className="bg-blue-600 text-white p-4">
+        <h1 className="text-xl font-bold">League Rank List</h1>
+      </header>
+    </>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <p>ovo je pocetak</p>
+      <h1 className="text-yellow-400 text-3xl font-bold">League Rank List</h1>
+    </>
+  );
+}
